3f29537fd5c1d00c0b30735080c3f9085289f10d
kenbrocode <ken.natigov@gmail.com>
2024-02-01 09:25:26 -0800

    initial

--- solvd-sql-university/target/classes/sql/agregations.sql
+++ solvd-sql-university/target/classes/sql/agregations.sql
@@ -25,7 +25,11 @@ left join proffessors_subjects pf_sb on pf.id = pf_sb.proffessor_id
 left join subjects sb on pf_sb.subject_id =  sb.id
 group by pf.name, pf.surname;
 
-
+select  hr.medical_report, count(al.title and vc.title)
+from health_records hr
+left join allergies al  on hr.id = al.health_record_id
+left join vaccines vc  on hr.id = vc.health_record_id
+group by hr.medical_report;
 
 select pm.data, avg(pr.cost)
 from prices pr
@@ -34,6 +38,12 @@ group by pm.data;
 
 
 
+
+select sb.title, count(pf.name and pf.surname) as proffessors_number
+from subjects sb 
+left join proffessors_subjects pf_sb on sb.id = pf_sb.subject_id
+left join proffessors pf on pf_sb.proffessor_id = pf.id
+group by sb.title having proffessors_number >=2 ;
 
 select st.name, count(ex.title) as exams_number
 from students st 
